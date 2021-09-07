4699a19b8259089cf4fb9e401cbb265e17218818
Owaap <ostap.cholach2@gmail.com>
2021-09-07 03:49:23 +0300

    beckup

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  isLoggedIn: boolean = false;
+  constructor(
+    public firebaseAuth: AngularFireAuth,
+    public firestore: AngularFirestore,
+    private router: Router
+  ) {}
+
+  signIn(email: string, password: string) {
+    this.firebaseAuth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error.code);
+      })
+      .then((res) => {
+        console.log(res?.user);
+        const userData = res?.user;
+        localStorage.setItem('user', JSON.stringify(userData));
+        const adminUid = 'sOcyTz1Ez2V6dzlXaBLXcmXXM182';
+
+        userData?.uid === adminUid
+          ? this.router.navigate(['admin'])
+          : this.router.navigate(['']);
+
+        // this.firestore.collection('users').add();
+      });
+  }
+
+  signUp(email: string, password: string) {
+    this.firebaseAuth
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error.code);
+      })
+      .then((res) => {
+        localStorage.setItem('user', JSON.stringify(res?.user));
+      });
+  }
+  signOut() {
+    this.firebaseAuth.signOut();
+    localStorage.removeItem('user');
+  }
 }
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserInfo } from '../models/user-info';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private firestore: AngularFirestore) {}
+  getUsers() {
+    return this.firestore.collection('users').snapshotChanges();
+  }
+  addUserInfo(userInfo: UserInfo) {
+    this.firestore.collection('users').add(userInfo);
+  }
+  editUserInfo(userInfo: UserInfo) {
+    // this.firestore.doc(`teas/${data.id}`).update(data);
+  }
+}
